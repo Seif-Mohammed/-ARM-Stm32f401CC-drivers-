540e869833b152e09260de9179e27dddf439e340
Seif Mohamed <134406973+Seif-Mohammed@users.noreply.github.com>
2023-08-18 02:49:15 +0300

    Add files via upload

---
+++ MRCC/RCC_Config.h
@@ -0,0 +1,34 @@
+/*
+ * RCC_Config.h
+ *
+ *  Created on: Aug 10, 2023
+ *      Author: seifm
+ */
+
+#ifndef MCAL_RCC_CONFIG_H_
+#define MCAL_RCC_CONFIG_H_
+
+
+
+	/*- CLKSRC option :
+			         1) HSI
+			         2) HSE
+			         3) PLL
+*/
+       #define CLKSRC  HSE
+/*
+   - HSE option :
+                 1)_HSE_CRYSTAL
+                 2)_HSE_RC
+*/
+
+       #define HSE_SRC  _HSE_CRYSTAL
+/*
+   - PLL option :
+	            1) _HSE_PLL
+                2) _HSI_PLL
+ */
+      #define PLL_SRC   _HSE_PLL
+
+
+#endif /* MCAL_RCC_CONFIG_H_ */
